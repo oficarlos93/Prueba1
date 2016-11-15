ef68bc9de640433ab803ec5d730bd2566c3e366d
oficarlos93 <carlos@Ofi.Local>
2016-11-15 17:59:02 +0100

    Primer controlador

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','portadaModulo'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,22 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('menu', {
+                url: '/menu',
+                templateUrl: 'templates/menu.html',
+                
+            })
+            .state('portada', {
+                url: '/portada',
+                templateUrl: 'templates/portada.html',
+                controller: 'controladorPortada'
+            })
+            
+        $urlRouterProvider.otherwise('/portada');
+    });
+function onclick($scope) {
+  $scope.mensaje="Hola Mundo";
+}
