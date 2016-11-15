b0ef42f6090ab2b2fd485e77846b3387fa70076e
Javier <javier@Ofi.Local>
2016-11-15 18:35:17 +0100

    add controladorMenu

---
+++ www/js/controladorMenu.js
@@ -0,0 +1,8 @@
+var app = angular.module("menuCtrl",[]);
+app.controller("listaCompra",["$scope",function($scope){
+
+$scope.listaCompra1 = [
+    'patatas','habichuelas','colacao','redbull'
+];
+
+}])
